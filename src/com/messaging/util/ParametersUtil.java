bbeee9ec928787c71df03a2286fd679a91f4abb7
ahamouda88 <ahmed.hamouda8888@gmail.com>
2015-12-09 14:32:33 -0500

    Adding a comment, and adding a synchronized block to the params

--- src/com/messaging/util/ParametersUtil.java
+++ src/com/messaging/util/ParametersUtil.java
@@ -1,14 +1,19 @@
 package com.messaging.util;
 
+/**
+ * This utility class is used to operate different functions on a method's parameters.
+ */
 public final class ParametersUtil {
 	
 	private ParametersUtil(){}
 	
 	public static void checkNullParameters(Object... paras){
-		if(paras != null){
-			for(Object o : paras){
-				if(o == null){
-					throw new NullPointerException();
+		synchronized(paras){
+			if(paras != null){
+				for(Object o : paras){
+					if(o == null){
+						throw new NullPointerException();
+					}
 				}
 			}
 		}
